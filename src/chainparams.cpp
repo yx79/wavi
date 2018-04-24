31f0fb1ba6fb587b78de90fedddf348bc4c24370
yx79 <yx79@georgetown.edu>
2018-04-24 19:22:45 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -72,7 +72,7 @@ class CMainParams : public CChainParams {
 public:
     CMainParams() {
         strNetworkID = "main";
-        consensus.nSubsidyHalvingInterval = 788400; // Note: actual number of blocks per calendar year with DGW v3 is ~200700 (for example 449750 - 249050)
+        consensus.nSubsidyHalvingInterval = 262800; // one year
         consensus.nMasternodePaymentsStartBlock = 1; // not true, but it's ok as long as it's less then nMasternodePaymentsIncreaseBlock
         consensus.nMasternodePaymentsIncreaseBlock = 158000; // actual historical value
         consensus.nMasternodePaymentsIncreasePeriod = 576*30; // 17280 - actual historical value
@@ -92,8 +92,8 @@ public:
         consensus.BIP34Height = 0;
         consensus.BIP34Hash = uint256S("0x00000d39b78c04cc35653abe2442a106a428c5e92ba41ec4967a80a09abf4725");//00000516a96541bf40e137db48544378afd997d0ed3c7dc6d007ab6e953499be
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000");
-        consensus.nPowTargetTimespan = 24 * 60 * 60; // Wavi: 1 day
-        consensus.nPowTargetSpacing = 2.0 * 60; // Wavi: 2.0 minutes
+        consensus.nPowTargetTimespan =  60 * 60; //  1 hour
+        consensus.nPowTargetSpacing = 2.5 * 60; //  2.5 minutes
         consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.fPowNoRetargeting = false;
         consensus.nPowKGWHeight = 0;
@@ -127,20 +127,20 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0x9f;
-        pchMessageStart[1] = 0xeb;
-        pchMessageStart[2] = 0x5a;
-        pchMessageStart[3] = 0xad;
+        pchMessageStart[0] = 0x1d;
+        pchMessageStart[1] = 0xab;
+        pchMessageStart[2] = 0x33;
+        pchMessageStart[3] = 0x4f;
         vAlertPubKey = ParseHex("04c6c4f3d2903b682e15deb0fda76639e12e4acbf931b4788142f003014b131ec71e7e6444e7e89bc137601e5811d002ba7887e9bb8681ff6b22dfc6ab17a2a3e8");
-        nDefaultPort = 9983;
+        nDefaultPort = 9998;
         nMaxTipAge = 6 * 60 * 60; // ~144 blocks behind -> 2 x fork detection time, was 24 * 60 * 60 in bitcoin
         nDelayGetHeadersTime = 24 * 60 * 60;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1520778900, 790796, 0x1e0ffff0, 1, 50 * COIN);//
+        genesis = CreateGenesisBlock(1520778900, 50000, 0x1e0ffff0, 1, 50 * COIN);//
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000d39b78c04cc35653abe2442a106a428c5e92ba41ec4967a80a09abf4725"));//0x00000516a96541bf40e137db48544378afd997d0ed3c7dc6d007ab6e953499be
-        assert(genesis.hashMerkleRoot == uint256S("0xa995d4dbdb823bd9dc5e2b6b5d6401f5255ebeae5ed200c6ad79d2c568c1b017"));
+        assert(consensus.hashGenesisBlock == uint256S("0x"));
+        assert(genesis.hashMerkleRoot == uint256S("0x"));
 
 
         vSeeds.clear();
@@ -176,27 +176,7 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-/*            (  1500, uint256S("0x000000aaf0300f59f49bc3e970bad15c11f961fe2347accffff19d96ec9778e3"))
-            (  4991, uint256S("0x000000003b01809551952460744d5dbb8fcbd6cbae3c220267bf7fa43f837367"))
-            (  9918, uint256S("0x00000000213e229f332c0ffbe34defdaa9e74de87f2d8d1f01af8d121c3c170b"))
-            ( 16912, uint256S("0x00000000075c0d10371d55a60634da70f197548dbbfa4123e12abfcbc5738af9"))
-            ( 23912, uint256S("0x0000000000335eac6703f3b1732ec8b2f89c3ba3a7889e5767b090556bb9a276"))
-            ( 35457, uint256S("0x0000000000b0ae211be59b048df14820475ad0dd53b9ff83b010f71a77342d9f"))
-            ( 45479, uint256S("0x000000000063d411655d590590e16960f15ceea4257122ac430c6fbe39fbf02d"))
-            ( 55895, uint256S("0x0000000000ae4c53a43639a4ca027282f69da9c67ba951768a20415b6439a2d7"))
-            ( 68899, uint256S("0x0000000000194ab4d3d9eeb1f2f792f21bb39ff767cb547fe977640f969d77b7"))
-            ( 74619, uint256S("0x000000000011d28f38f05d01650a502cc3f4d0e793fbc26e2a2ca71f07dc3842"))
-            ( 75095, uint256S("0x0000000000193d12f6ad352a9996ee58ef8bdc4946818a5fec5ce99c11b87f0d"))
-            ( 88805, uint256S("0x00000000001392f1652e9bf45cd8bc79dc60fe935277cd11538565b4a94fa85f"))
-            ( 107996, uint256S("0x00000000000a23840ac16115407488267aa3da2b9bc843e301185b7d17e4dc40"))
-            ( 137993, uint256S("0x00000000000cf69ce152b1bffdeddc59188d7a80879210d6e5c9503011929c3c"))
-            ( 167996, uint256S("0x000000000009486020a80f7f2cc065342b0c2fb59af5e090cd813dba68ab0fed"))
-            ( 207992, uint256S("0x00000000000d85c22be098f74576ef00b7aa00c05777e966aff68a270f1e01a5"))
-            ( 312645, uint256S("0x0000000000059dcb71ad35a9e40526c44e7aae6c99169a9e7017b7d84b1c2daf"))
-            ( 407452, uint256S("0x000000000003c6a87e73623b9d70af7cd908ae22fee466063e4ffc20be1d2dbc"))
-            ( 523412, uint256S("0x000000000000e54f036576a10597e0e42cc22a5159ce572f999c33975e121d4d"))
-            ( 523930, uint256S("0x0000000000000bccdb11c2b1cfb0ecab452abf267d89b7f46eaf2d54ce6e652c"))*/
-            ( 0, uint256S("0x00000d39b78c04cc35653abe2442a106a428c5e92ba41ec4967a80a09abf4725")),//00000516a96541bf40e137db48544378afd997d0ed3c7dc6d007ab6e953499be
+            ( 0, uint256S("0x")),//00000516a96541bf40e137db48544378afd997d0ed3c7dc6d007ab6e953499be
             1520778900, //1519569124 * UNIX timestamp of last checkpoint block
             0,    // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
@@ -311,8 +291,6 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-/*            (    261, uint256S("0x00000c26026d0815a7e2ce4fa270775f61403c040647ff2c3091f99e894a4618"))
-            (   1999, uint256S("0x00000052e538d27fa53693efe6fb6892a0c1d26c0235f599171c48a3cce553b1"))*/
             (   0, uint256S("0x00000ac144dd1c55a66b6486d96597fd624b9a072fa3572b9457eda66f0f740f")),//00000a97fc9bb3c18349014c348debd9b7d134a48b89c16e4edb63a79a91b863
 
             1520778910, //1519569150 * UNIX timestamp of last checkpoint block
